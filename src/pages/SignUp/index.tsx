3bcdc3e9bc41b8a9d4b212a628011bad85cee0d2
douglasdsda <douglasdsda@gmail.com>
2020-07-08 10:09:59 -0300

    tooltip 2

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -14,7 +14,7 @@ import Button from '../../components/Button';
 export const SignUp: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: Record<string, unknown>) => {
     try {
       formRef.current?.setErrors({});
 
